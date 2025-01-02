8d9f30e62bd454c695ae2d2465a526864803a6a3
Alex Evans <56258586+alexevans-dotcom@users.noreply.github.com>
2025-01-02 14:11:56 +0300

    Add files via upload

---
+++ components/HapticTab.tsx
@@ -0,0 +1,10 @@
+import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
+import { PlatformPressable } from '@react-navigation/elements';
+export function HapticTab(props: BottomTabBarButtonProps) {
+  return (
+    <PlatformPressable
+      {...props}
+  
+    />
+  );
+}
